15dc0eb91d7cd108ae90477f9c8fd7d3ad346e69
hairbarberrevolution <nicolofarinella79@gmail.com>
2026-07-11 17:21:24 +0200

    Add files via upload

---
+++ promozionidonna.js
@@ -0,0 +1,12 @@
+window.PROMOZIONI_DONNA_DATA = [
+  {
+    "nome": "PIEGA DONNA IN PROMOZIONE",
+    "descrizione": "Questo mese la piega donna è in promozione a soli 15€, per un risultato impeccabile ad un prezzo speciale.",
+    "foto": "piegadonna.jpg"
+  },
+  {
+    "nome": "COLORE + TRATTAMENTO IN PROMOZIONE",
+    "descrizione": "Prenota il colore completo con trattamento ristrutturante incluso e risparmia il 20% sul prezzo totale.",
+    "foto": "coloredonna.jpg"
+  }
+];
---
+++ promozioniuomo.js
@@ -0,0 +1,1 @@
+window.PROMOZIONI_UOMO_DATA = [];
