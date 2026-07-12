566931831cbd92c41d6490cee682f9867fad0b4d
hairbarberrevolution <nicolofarinella79@gmail.com>
2026-07-12 15:51:44 +0200

    Enhance promotion description with links
    Updated the description with clickable links for Google and WhatsApp.

--- promozionidonna.js
+++ promozionidonna.js
@@ -1,6 +1,7 @@
 window.PROMOZIONI_DONNA_DATA = [
   {
   "nome": "10% DI SCONTO SE LASCI UNA RECENSIONE",
-"descrizione": "✨ Solo per Luglio 2026 e valido per il reparto femminile ✨ Lascia una recensione al nostro salone su Google e inviaci lo screenshot su WhatsApp: riceverai il 10% di sconto sul tuo prossimo trattamento in salone! Ti aspettiamo! 💈✂️",  "foto": "promozione_luglio_recensioni.png"
+  "descrizione": "✨ Solo per Luglio 2026 e valido per il reparto femminile ✨ Lascia una recensione al nostro salone su <a href=\"https://g.page/r/CX5BLAXF88S0EBM/review\" target=\"_blank\" style=\"text-decoration: underline; font-weight: bold;\">Google</a> e inviaci lo screenshot su <a href=\"https://wa.me/393331355200\" target=\"_blank\" style=\"text-decoration: underline; font-weight: bold;\">WhatsApp</a>: riceverai il 10% di sconto sul tuo prossimo trattamento in salone! Ti aspettiamo! 💈✂️",
+  "foto": "promozione_luglio_recensioni.png"
 }
 ];
