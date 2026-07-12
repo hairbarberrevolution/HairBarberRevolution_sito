f4089e41ae4773ac7e1c562442006b4b3c0b7ae9
hairbarberrevolution <nicolofarinella79@gmail.com>
2026-07-12 15:40:37 +0200

    Add promotional discount for reviews in promozionidonna.js

--- promozionidonna.js
+++ promozionidonna.js
@@ -1,1 +1,6 @@
-window.PROMOZIONI_DONNA_DATA = [];
+window.PROMOZIONI_DONNA_DATA = [
+  {
+  "nome": "10% DI SCONTO SE LASCI UNA RECENSIONE",
+"descrizione": "✨ Solo per Luglio 2026 e valido per il reparto femminile ✨ Lascia una recensione al nostro salone su Google e inviaci lo screenshot su WhatsApp: riceverai il 10% di sconto sul tuo prossimo trattamento in salone! Ti aspettiamo! 💈✂️",  "foto": "promozione_luglio_recensioni.png"
+}
+];
