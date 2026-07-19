17800b7bc9f33f31a52a7fa6ea22ea3aac7c4b66
hairbarberrevolution <nicolofarinella79@gmail.com>
2026-07-19 22:33:04 +0200

    Update event date from July 2025 to July 2026

--- NOVITA.js
+++ NOVITA.js
@@ -13,7 +13,7 @@
 window.NOVITA_DATA = [
   {
     "titolo": "Campioni dei Campioni d'Italia 2025 🏆",
-    "data": "Luglio 2025",
+    "data": "19 Luglio 2026",
     "descrizione": "Un traguardo che tengo nel cuore e che voglio condividere con voi.\n\nSono orgogliosa di annunciarvi che sono stata selezionata tra i <strong>Campioni dei Campioni d'Italia 2025</strong> nella categoria <em>Femminile – Taglio Medio, Colore e Piega</em> 🇮🇹✨\n\nUn riconoscimento assegnato da <strong>Il Giornale del Parrucchiere</strong>, che nel 2025 ha visto la partecipazione di ben <strong>1.014 professionisti</strong> da tutta Italia ai propri contest mensili. Essere tra i 40 selezionati a livello nazionale è per me motivo di grande orgoglio.\n\nMa questo risultato non sarebbe stato possibile senza di voi. <strong>Grazie di cuore a tutte le mie clienti</strong>: ogni giorno mi date la fiducia, l'affetto e la possibilità di fare il lavoro che amo. Questo traguardo è anche vostro. ❤️\n\n<em>Con affetto,<br>Veruska Toma<br>Hair&amp;Barber Revolution</em>",
     "foto": "campioni_italia_2025.png"
   },
