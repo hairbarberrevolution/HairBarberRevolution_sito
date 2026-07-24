dc4afa03afd1ccf1fa57f9a4e1b27acf1999c852
hairbarberrevolution <nicolofarinella79@gmail.com>
2026-07-24 18:33:20 +0200

    Add new promotion for waiting review discount

--- promozionidonna.js
+++ promozionidonna.js
@@ -1,5 +1,13 @@
 window.PROMOZIONI_DONNA_DATA = [
-  {
+
+{
+    "nome": "⏳ Qualche minuto di attesa? Trasformalo in uno sconto!",
+    "descrizione": "Nella nostra sala d’attesa trovi un QR code che ti permette di lasciare una recensione in meno di un minuto. È il tuo modo di aiutarci a crescere e, come ringraziamento, riceverai un 10% di sconto. Se hai bisogno di una mano, trovi il <a href=\"VIDEO_HBR/tutorial_recensione.MP4\" target=\"_blank\" style=\"text-decoration: underline; font-weight: bold;\">video tutorial consultabile qui</a>.",
+    "video": "recensione_attesa.mp4"
+
+},
+
+{
   "nome": "10% DI SCONTO SE LASCI UNA RECENSIONE",
   "descrizione": "✨ Solo per Luglio 2026 e valido per il reparto femminile ✨ Lascia una recensione al nostro salone su <a href=\"https://g.page/r/CX5BLAXF88S0EBM/review\" target=\"_blank\" style=\"text-decoration: underline; font-weight: bold;\">Google</a> e inviaci lo screenshot su <a href=\"https://wa.me/393331355200\" target=\"_blank\" style=\"text-decoration: underline; font-weight: bold;\">WhatsApp</a>: riceverai il 10% di sconto sul tuo prossimo trattamento in salone! Ti aspettiamo! 💈✂️",
   "foto": "promozione_luglio_recensioni.png"
